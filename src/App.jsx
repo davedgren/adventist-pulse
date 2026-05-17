23873ec3b4b389dc3793874e5da62fa3c06edf6d
davedgren <davedgren@gmail.com>
2026-05-17 17:05:45 +1000

    Remove Firebase SDK imports from App.jsx
    Removed Firebase SDK imports and related comments.

--- src/App.jsx
+++ src/App.jsx
@@ -30,14 +30,6 @@ import {
   Send
 } from 'lucide-react';
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCCC6G1iTb_OLAgBhPN7TqmoUh7Clj2QWU",
   authDomain: "adventist-pulse.firebaseapp.com",
